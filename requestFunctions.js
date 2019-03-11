7a894a827d4619fb51c13c88b50910fbbdbbab4b
Josh Whelan <josh.d.whelan@gmail.com>
2019-03-11 17:01:33 +0000

    wrote: getAndPrintHTMLChunks()

---
+++ requestFunctions.js
@@ -0,0 +1,28 @@
+var https = require('https');    
+
+
+function getAndPrintHTMLChunks () {
+    
+    var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+    };
+
+    https.get(requestOptions, function (response) {
+
+        response.setEncoding('utf8');
+      
+        response.on('data', function (data) {
+          console.log(data, '\n');
+        });
+      
+        response.on('end', function() {
+          console.log('Response stream complete.');
+        });
+      
+      });
+    
+  
+  }
+
+getAndPrintHTMLChunks();
